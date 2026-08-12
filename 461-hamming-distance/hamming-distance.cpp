04fe90faad1b7cc58bf3bbf11a36f6a459bd0f4f
Vishwanaren <vishwanaren2029@gmail.com>
2026-08-12 23:29:05 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (4.58%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int xorval = x^y;
+        int count = 0;
+        while(xorval > 0){
+            xorval &= xorval-1;
+            count++;
+        }
+        return count;
+    }
+};
