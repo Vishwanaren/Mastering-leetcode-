45bebc780ea3e2ea8a45a82223c0f2482ddf9cb0
Vishwanaren <vishwanaren2029@gmail.com>
2026-08-31 23:04:17 +0530

    Time: 4 ms (38.45%) | Memory: 12 MB (29.70%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map <char , int> mag;
+        for(char ch : magazine){
+            mag[ch]++;
+        }
+        for(char ch : ransomNote){
+            if(mag[ch] == 0){
+                return false;
+            }
+            else{
+                mag[ch]--;
+            }
+        }
+        return true;
+    }
+};
