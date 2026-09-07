bbcbe0e4cc37fc4159dec62cd3c0563afe42a8cf
Vishwanaren <vishwanaren2029@gmail.com>
2026-09-07 14:11:11 +0530

    Time: 112 ms (42.49%) | Memory: 142.2 MB (8.09%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -3,8 +3,10 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int,int>lastseen;
         for(int i =0;i<nums.size();i++){
-            if(lastseen.count(nums[i]) && (i-lastseen[nums[i]]) <= k){
+            if(lastseen.count(nums[i])){
+                if(i-lastseen[nums[i]] <= k){
                 return true;
+                }
             }
             lastseen[nums[i]] = i;
         }
