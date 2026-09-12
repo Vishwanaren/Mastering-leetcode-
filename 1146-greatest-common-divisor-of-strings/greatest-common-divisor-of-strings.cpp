0b8c2b88dc393b92b15e2539fd72e975d1a99e68
Vishwanaren <vishwanaren2029@gmail.com>
2026-09-12 08:41:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (62.95%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if(str1+str2 != str2+str1){
+           if(str1+str2!=str2+str1)
+           {
             return "";
-        }
-        int gcdlength = std::gcd(str1.length() , str2.length());
-        return str1.substr(0,gcdlength);
+           }
+           int len=gcd(str1.length(),str2.length());
+           return str1.substr(0,len);
     }
 };
